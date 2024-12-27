9e058487188fb8532bf1a4db54545abd2847f48c
25nyak <56729621+25nyak@users.noreply.github.com>
2024-12-27 15:55:38 -0600

    Update PluginManager.js

--- src/PluginManager/PluginManager.js
+++ src/PluginManager/PluginManager.js
@@ -1,21 +1,24 @@
+const MoviePlugin = require('../plugins/0123MoviePlugin');
+
 class PluginManager {
-  constructor(plugins){
+  constructor(plugins = [new MoviePlugin()]) {
     this.plugins = plugins;
   }
-  async getMovies(page){
+
+  async getMovies(page) {
     const res = await Promise.all(this.plugins.map(plugin => plugin.getMovies(page)));
     return res;
-  };
+  }
 
-  async getTVSeries(page){
+  async getTVSeries(page) {
     const res = await Promise.all(this.plugins.map(plugin => plugin.getTVSeries(page)));
     return res;
-  };
+  }
 
-  async getVideoURL(url){
+  async getVideoURL(url) {
     const res = await Promise.all(this.plugins.map(plugin => plugin.getVideoURL(url)));
     return res;
-  };
-};
+  }
+}
 
 module.exports = PluginManager;
